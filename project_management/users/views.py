0debde726f69234b9258aed433a2954220d263ee
Nathan Fekade <nathanfekade276@gmail.com>
2025-01-27 21:29:07 +0300

    created userviewset

--- project_management/users/views.py
+++ project_management/users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from users.serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
