6889986c325b49d8d9b977b4f76d284a196de82f
Nathan Fekade <nathanfekade276@gmail.com>
2025-01-27 21:27:45 +0300

    created users url and tasks views

--- project_management/tasks/views.py
+++ project_management/tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from tasks.models import Task
+from tasks.serializers import TasksSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TasksSerializer
+    
