0faa7928777f178336903b70d5e7812a212bef5b
Nathan Fekade <nathanfekade276@gmail.com>
2025-01-27 21:25:43 +0300

    created task model and added users to installed apps

--- project_management/tasks/models.py
+++ project_management/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    user = models.ForeignKey('auth.User', related_name='tasks', on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
