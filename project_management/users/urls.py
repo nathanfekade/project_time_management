0b474e6e5e985b2e1b3eff67d4e461a2bfb086b5
Nathan Fekade <nathanfekade276@gmail.com>
2025-02-03 12:20:27 +0300

    updated user url link and added migrations in gitignore

--- project_management/users/urls.py
+++ project_management/users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from users import views
 
 router = DefaultRouter()
-router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'', views.UserViewSet, basename='user')
 
 urlpatterns = [
     path('',include(router.urls))
