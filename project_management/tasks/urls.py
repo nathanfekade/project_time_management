61ed0e5fd9eeee76fdd6b420774fa4cc9edcb33d
Nathan Fekade <nathanfekade276@gmail.com>
2025-02-04 17:06:51 +0300

    url patterns changed to use class based views

--- project_management/tasks/urls.py
+++ project_management/tasks/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from tasks import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-router = DefaultRouter()
-router.register(r'tasks', views.TaskViewSet, basename='task')
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('tasks/', views.TaskList.as_view(), name='task-list'),
+    path('tasks/<int:pk>/', views.TaskDetail.as_view(), name='task-detail')
 ]
+
+#should i include it
+# urlpatterns = format_suffix_patterns(urlpatterns)
