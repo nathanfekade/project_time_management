7f7ad25993968231dbcc65b36e3ed24747e0d9c7
Nathan Fekade <nathanfekade276@gmail.com>
2025-02-04 17:05:56 +0300

    changed Hyperlinkedserializer to ModelSerializer

--- project_management/tasks/serializers.py
+++ project_management/tasks/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from tasks.models import Task
 
-class TasksSerializer(serializers.HyperlinkedModelSerializer):
+class TasksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['url','title','description','user','completed','created_at']
+        fields = ['id','title','description','user','completed','created_at']
+
+# class CategoriesSerializer(serializers.HyperlinkedModelSerializer):
+    
+#     class Meta:
+#         model = Categories
+#         fields = ['url', 'title', 'tasks']
