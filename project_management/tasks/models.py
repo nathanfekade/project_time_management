7c6be6040036a8d0c6da5c88b7cdbe8ca841aa36
Nathan Fekade <nathanfekade276@gmail.com>
2025-02-13 09:33:54 +0300

    check unique_together

--- project_management/tasks/models.py
+++ project_management/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Task(models.Model):
@@ -10,10 +11,33 @@ class Task(models.Model):
 
     class Meta:
         ordering = ['created_at']
+        unique_together = ['user', 'title']
 
-class Categories(models.Model):
-    title = models.CharField(max_length=100)
-    task = models.ManyToManyField(Task)
+    
+    def save(self, *args, **kwargs):
+        self.full_clean() # validating in both models and serializers incase data was inserted with out using DRF serializsers
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
 
-    class meta:
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    tasks = models.ManyToManyField(Task, related_name='categories')
+    user = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
+
+    class Meta:
         ordering = ['title']
+        unique_together = ['user','title' ]
+
+
+    
+    def save(self, *args, **kwargs):
+        self.full_clean() # validating in both models and serializers incase data was inserted with out using DRF serializsers
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+
+
