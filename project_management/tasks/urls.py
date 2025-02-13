c62c158d6a5bc9607260d5ccd1d042d68463ec8a
Nathan Fekade <nathanfekade276@gmail.com>
2025-02-13 09:37:13 +0300

    urls for crud of category and also url fo category task relationship

--- project_management/tasks/urls.py
+++ project_management/tasks/urls.py
@@ -5,8 +5,30 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('tasks/', views.TaskList.as_view(), name='task-list'),
-    path('tasks/<int:pk>/', views.TaskDetail.as_view(), name='task-detail')
+    path('tasks/<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
+    path('category/', views.CategoryList.as_view(), name='category-list'),
+    path('category/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
+    path('categories/<int:category_id>/add-tasks', views.AddTasksToCategory.as_view(), name='add-tasks-to-category')
 ]
+
 
 #should i include it
 # urlpatterns = format_suffix_patterns(urlpatterns)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
